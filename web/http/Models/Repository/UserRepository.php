836dea0625da4e53cf71790fd10e663abcdedabe
dossantoscarlos <anjoazrael10@gmail.com>
2018-11-21 02:49:36 -0200

    alterando autoloader

--- web/http/Models/Repository/UserRepository.php
+++ web/http/Models/Repository/UserRepository.php
@@ -1,9 +1,9 @@
 <?php 
 
-namespace \Web\Entity\Repository\;
+namespace Web\Http\Entity\Repository\;
 
 use Doctrine\ORM\EntityRepository;
-use Web\Entity\User;
+use Web\Http\Models\Entity\User;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\ORMInvalidArgumentException;
 use Doctrine\ORM\ORMException;

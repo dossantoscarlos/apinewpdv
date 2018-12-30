5761ec4e279370debd06a181335812cab56b2068
dossantoscarlos <anjoazrael10@gmail.com>
2018-12-30 01:39:28 -0200

    middlewares

--- src/middleware.php
+++ src/middleware.php
@@ -8,13 +8,13 @@ use Web\Middlewares\RouterSeparator;
 use Web\Middlewares\RegisterMediaType;
 use Web\Middlewares\AccessApi;
 use Web\Middlewares\RegisterMethodRoute;
+use Tuupola\Middleware\HttpBasicAuthentication;
 
 //token
 $app->add(new Guard);
 
 //remove a / do final da url
 $app->add( new RouterSeparator);
-
 
 $app->add(new RegisterMediaType);
 
@@ -25,20 +25,20 @@ $app->add(new AccessApi);
 $app->add(new RegisterMethodRoute);
 
 //basic-http-auth
-//
-// $app->add(new Tuupola\Middleware\HttpBasicAuthentication([
+// $app->add(new HttpBasicAuthentication([
 //     "path" => "/",
 //     "realm" => "Protected",
 //    "users" => [
-//         "username" => "t00r",
-//         "password" => "passw0rd"
+//         "username" => "carlos",
+//         "password" => "1004"
 //     ],
 //     "error" => function ($response, $arguments) {
 //         $data = [];
 //         $data["status"] = "error";
 //         $data["message"] = $arguments["message"];
 //         return $response->write(json_encode($data, JSON_UNESCAPED_SLASHES));
+//     },
+//     "done" => function ($response, $arguments){
+//         return redirect('/users');
 //     }
 // ]));
-
-//
